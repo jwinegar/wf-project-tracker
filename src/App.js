b796730bd21110e30a13972753b4d058d9f642c0
Jeff Winegar <jeff.winegar@marketingdirectla.com>
2019-10-24 17:51:36 -0700

    moves context to index render

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from "react";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "@apollo/react-hooks";
 
 import ProjectFilters from "./components/ProjectFilters";
 import ProjectsList from "./components/ProjectsList";
-import { ContextProvider } from "./globalState/state";
-
-const client = new ApolloClient({
-  uri: "http://localhost:2045/graphql"
-});
 
 const App = () => (
-  <ApolloProvider client={client}>
-    <ContextProvider>
-      <div className="App">
-        <ProjectFilters />
-        <ProjectsList />
-      </div>
-    </ContextProvider>
-  </ApolloProvider>
+  <div className="App">
+    <ProjectFilters />
+    <ProjectsList />
+  </div>
 );
 
 export default App;
